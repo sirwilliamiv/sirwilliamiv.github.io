f8c6d9da7fa2fbb17f7a31b517c01ddfadd2bb21
sirwilliamiv <wjconnolly2@gmail.com>
2016-11-27 13:46:31 -0600

    coded myself into a corner. started with a fresh plate of three columns

--- js/site.js
+++ js/site.js
@@ -5,36 +5,74 @@
 // 
 
 
-
+// Currently enrolled to be a Full-Stack Software Developer
 
 
 console.log("Hey, I work");
 
  $(document).ready(function(){
 
-
-         $(function(){
-        $("h2 .first").typed({
-            strings: ["Technologies in progress: "],
-            typeSpeed: 30,
-            callback: function() {
-              showThis();
-            },
+//   // intro typing
+//          $(function(){
+//         $("p.first").typed({
+//             strings: ["Currently enrolled to be a Full-Stack Software Developer at Nashville Software School. I will not be where I was yesterday."],
+//             typeSpeed: 10,
+//             callback: function() {
+//               showThis();
+//             },
           
-        });
+//         });
    
-     function showThis(){
+//      function showThis(){
 
-        $("span.second").typed({
-              strings: ["JavaScript", "html5", "css3", "Sass", "Coffee","Firebase","Bootstrap", "jQuery", "Node.js", "Ajax", "Angular", "expressjs", "Atom", "Grunt", "Mocha", "Chai"],
-              backDelay: 1500,    
-              typeSpeed: 10,
-              backSpeed: 0,
-              loop: true,
-            });
+//         $("span.second").typed({
+//               strings: ["JavaScript", "HTML5", "CSS3", "Sass", "Coffee","Firebase","Bootstrap", "jQuery", "Node.js", "Ajax", "AngularJS", "Express.js", "Atom", "Grunt", "Mocha", "Chai"],
+//               // backDelay: 1500,    
+//               typeSpeed: 40,
+//               backSpeed: 20,
+//               loop: true,
+//             });
 
-         }
-    });
+//          }
+//     });
+
+// // .quote  , .skills
+// // 
+//     $(function(){
+//         $(".quote").typed({
+//             strings: [" I will not be where I was yesterday."],
+//             typeSpeed: 10,
+//             callback: function() {
+//               showThis();
+//             },
+          
+//         });
+   
+//      function showThis(){
+
+//         $(".skills").typed({
+//               strings: ["JavaScript", "HTML5", "CSS3", "Sass", "Coffee","Firebase","Bootstrap", "jQuery", "Node.js", "Ajax", "AngularJS", "Express.js", "Atom", "Grunt", "Mocha", "Chai"],
+//               // backDelay: 1500,    
+//               typeSpeed: 40,
+//               backSpeed: 20,
+//               loop: true,
+//             });
+
+//          }
+//     });
+//     
+       setTimeout(function(){
+        $("span.first").css("display", "inherit");
+        $("span.first").typed({
+            strings: ["yes ykles"],
+            typeSpeed: 10, // typing speed
+            backDelay: 1000, // pause before backspacing
+            loop: true, // loop on or off (true or false)
+            loopCount: false, // number of loops, false = infinite
+            callback: function(){ } // call function after typing is done
+        });
+    }, 200);
+
 
 
 
